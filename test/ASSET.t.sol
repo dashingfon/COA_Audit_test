2024b7c6dadf176cfe04265e3f0bb96cbd8bc111
Mfon Nwa <mfonnwa@gmail.com>
2025-01-17 08:39:12 +0100

    finished the unit tests

--- test/ASSET.t.sol
+++ test/ASSET.t.sol
@@ -213,4 +213,302 @@ contract ASSET_Test is Test {
         assert(_acre.freeParticipant(free_participant) == true);
     }
 
+    function testPLOT_mint() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _plot.setCurrentBatch(1, 1, true);
+        vm.prank(bob);
+        busd.approve(address(_plot), 10 ether);
+
+        // execute
+        vm.prank(bob);
+        _plot.mint(1);
+
+        // validate
+        assert(_plot.balanceOf(bob) == 1);
+        assert(_plot.ownerOf(0) == bob);
+    }
+
+    function testPLOT_TransferFromfail() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _plot.setCurrentBatch(1, 1, true);
+
+        // execute and check
+        vm.startPrank(bob);
+        vm.expectRevert();
+        _plot.transferFrom(bob, eve, 0);
+        vm.stopPrank();
+    }
+
+    function testPLOT_TransferFrom() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _plot.setCurrentBatch(1, 1, true);
+        vm.startPrank(bob);
+        busd.approve(address(_plot), 10 ether);
+        _plot.mint(1);
+        vm.stopPrank();
+
+        // execute
+        vm.prank(bob);
+        _plot.transferFrom(bob, eve, 0);
+
+        // validate
+        assert(_plot.balanceOf(eve) == 1);
+        assert(_plot.ownerOf(0) == eve);
+    }
+
+    function testPLOT_SettingParams() external {
+        // execute
+        bool active_set = false;
+        uint256 tx_fee = 10;
+        address payment_token = address(busd);
+        address fee_collector = address(this);
+        address free_participant_controller = address(this);
+        address free_participant = address(this);
+
+        _plot.setCurrentBatchActive(active_set);
+        _plot.setTxFee(tx_fee);
+        _plot.setPaymentToken(payment_token);
+        _plot.setFeeCollector(fee_collector);
+        _plot.setFreeParticipantController(free_participant_controller, true);
+        _plot.setFreeParticipant(free_participant, true);
+
+        // check
+        (,, bool active) = _plot._currentBatch();
+
+        // validate
+        assert(active == active_set);
+        assert(_plot._paymentToken() == payment_token);
+        assert(_plot._feeCollector() == fee_collector);
+        assert(_plot.freeParticipantControllers(free_participant_controller) == true);
+        assert(_plot.freeParticipant(free_participant) == true);
+    }
+
+    function testPLOTV2_mint() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _plotV2.setCurrentBatch(1, 1, true);
+        vm.prank(bob);
+        busd.approve(address(_plotV2), 10 ether);
+
+        // execute
+        vm.prank(bob);
+        _plotV2.mint(1);
+
+        // validate
+        assert(_plotV2.balanceOf(bob) == 1);
+        assert(_plotV2.ownerOf(0) == bob);
+    }
+
+    function testPLOTV2_TransferFromfail() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _plotV2.setCurrentBatch(1, 1, true);
+
+        // execute and check
+        vm.startPrank(bob);
+        vm.expectRevert();
+        _plotV2.transferFrom(bob, eve, 0);
+        vm.stopPrank();
+    }
+
+    function testPLOTV2_TransferFrom() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _plotV2.setCurrentBatch(1, 1, true);
+        vm.startPrank(bob);
+        busd.approve(address(_plotV2), 10 ether);
+        _plotV2.mint(1);
+        vm.stopPrank();
+
+        // execute
+        vm.prank(bob);
+        _plotV2.transferFrom(bob, eve, 0);
+
+        // validate
+        assert(_plotV2.balanceOf(eve) == 1);
+        assert(_plotV2.ownerOf(0) == eve);
+    }
+
+    function testPLOTV2_SettingParams() external {
+        // prepare
+        _plotV2.grantRole(keccak256("SIGNER_ROLE"), address(this));
+
+        // execute
+        bool active_set = false;
+        uint256 tx_fee = 10;
+        address payment_token = address(busd);
+        address fee_collector = address(this);
+        address free_participant_controller = address(this);
+        address free_participant = address(this);
+
+        _plotV2.setCurrentBatchActive(active_set);
+        _plotV2.setTxFee(tx_fee);
+        _plotV2.setPaymentToken(payment_token);
+        _plotV2.setFeeCollector(fee_collector);
+        _plotV2.setFreeParticipantController(free_participant_controller, true);
+        _plotV2.setFreeParticipant(free_participant, true);
+
+        // check
+        (,,bool active,,) = _plotV2._currentBatch();
+
+        // validate
+        assert(active == active_set);
+        assert(_plotV2._paymentToken() == payment_token);
+        assert(_plotV2._feeCollector() == fee_collector);
+        assert(_plotV2.freeParticipantControllers(free_participant_controller) == true);
+        assert(_plotV2.freeParticipant(free_participant) == true);
+    }
+ 
+    function testYARD_mint() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _yard.setCurrentBatch(1, 1, true);
+        vm.prank(bob);
+        busd.approve(address(_yard), 10 ether);
+
+        // execute
+        vm.prank(bob);
+        _yard.mint(1);
+
+        // validate
+        assert(_yard.balanceOf(bob) == 1);
+        assert(_yard.ownerOf(0) == bob);
+    }
+
+    function testYARD_TransferFromfail() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _yard.setCurrentBatch(1, 1, true);
+
+        // execute and check
+        vm.startPrank(bob);
+        vm.expectRevert();
+        _yard.transferFrom(bob, eve, 0);
+        vm.stopPrank();
+    }
+
+    function testYARD_TransferFrom() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _yard.setCurrentBatch(1, 1, true);
+        vm.startPrank(bob);
+        busd.approve(address(_yard), 10 ether);
+        _yard.mint(1);
+        vm.stopPrank();
+
+        // execute
+        vm.prank(bob);
+        _yard.transferFrom(bob, eve, 0);
+
+        // validate
+        assert(_yard.balanceOf(eve) == 1);
+        assert(_yard.ownerOf(0) == eve);
+    }
+
+    function testYARD_SettingParams() external {
+        // execute
+        bool active_set = false;
+        uint256 tx_fee = 10;
+        address payment_token = address(busd);
+        address fee_collector = address(this);
+        address free_participant_controller = address(this);
+        address free_participant = address(this);
+
+        _yard.setCurrentBatchActive(active_set);
+        _yard.setTxFee(tx_fee);
+        _yard.setPaymentToken(payment_token);
+        _yard.setFeeCollector(fee_collector);
+        _yard.setFreeParticipantController(free_participant_controller, true);
+        _yard.setFreeParticipant(free_participant, true);
+
+        // check
+        (,, bool active) = _yard._currentBatch();
+
+        // validate
+        assert(active == active_set);
+        assert(_yard._paymentToken() == payment_token);
+        assert(_yard._feeCollector() == fee_collector);
+        assert(_yard.freeParticipantControllers(free_participant_controller) == true);
+        assert(_yard.freeParticipant(free_participant) == true);
+    }
+
+    function testYARDV2_mint() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _yardV2.setCurrentBatch(1, 1, true);
+        vm.prank(bob);
+        busd.approve(address(_yardV2), 10 ether);
+
+        // execute
+        vm.prank(bob);
+        _yardV2.mint(1);
+
+        // validate
+        assert(_yardV2.balanceOf(bob) == 1);
+        assert(_yardV2.ownerOf(0) == bob);
+    }
+
+    function testYARDV2_TransferFromfail() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _yardV2.setCurrentBatch(1, 1, true);
+
+        // execute and check
+        vm.startPrank(bob);
+        vm.expectRevert();
+        _yardV2.transferFrom(bob, eve, 0);
+        vm.stopPrank();
+    }
+
+    function testYARDV2_TransferFrom() external {
+        // prepare
+        busd.mint(bob, 100 ether);
+        _yardV2.setCurrentBatch(1, 1, true);
+        vm.startPrank(bob);
+        busd.approve(address(_yardV2), 10 ether);
+        _yardV2.mint(1);
+        vm.stopPrank();
+
+        // execute
+        vm.prank(bob);
+        _yardV2.transferFrom(bob, eve, 0);
+
+        // validate
+        assert(_yardV2.balanceOf(eve) == 1);
+        assert(_yardV2.ownerOf(0) == eve);
+    }
+
+    function testYARDV2_SettingParams() external {
+        // prepare
+        _yardV2.grantRole(keccak256("SIGNER_ROLE"), address(this));
+
+        // execute
+        bool active_set = false;
+        uint256 tx_fee = 10;
+        address payment_token = address(busd);
+        address fee_collector = address(this);
+        address free_participant_controller = address(this);
+        address free_participant = address(this);
+
+        _yardV2.setCurrentBatchActive(active_set);
+        _yardV2.setTxFee(tx_fee);
+        _yardV2.setPaymentToken(payment_token);
+        _yardV2.setFeeCollector(fee_collector);
+        _yardV2.setFreeParticipantController(free_participant_controller, true);
+        _yardV2.setFreeParticipant(free_participant, true);
+
+        // check
+        (,,bool active,,) = _yardV2._currentBatch();
+
+        // validate
+        assert(active == active_set);
+        assert(_yardV2._paymentToken() == payment_token);
+        assert(_yardV2._feeCollector() == fee_collector);
+        assert(_yardV2.freeParticipantControllers(free_participant_controller) == true);
+        assert(_yardV2.freeParticipant(free_participant) == true);
+    }
+ 
 }
